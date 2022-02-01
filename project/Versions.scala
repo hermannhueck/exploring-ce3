9a6e7a51ae68a196e6b5280b7358eb9a76d48425
Hermann Hueck <hermann.hueck@mailbox.org>
2022-02-01 08:33:17 +0100

    build: bump versions: sbt 1.6.2, CE3 3.3.5, scalafmt 3.4.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -7,9 +7,9 @@ object Versions {
   val scala2Version = "2.13.8"
 
   // 3rd party libraries
-  val catsEffectVersion = "3.3.4"
-  val munitCe3Version  = "1.0.7"
-  val weaverCatsVersion  = "0.7.9"
+  val catsEffectVersion = "3.3.5"
+  val munitCe3Version   = "1.0.7"
+  val weaverCatsVersion = "0.7.9"
 
   // compiler plugins
   val kindProjectorVersion    = "0.13.2"
